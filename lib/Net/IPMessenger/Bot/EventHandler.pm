139400e537aa4ad258b1076acd7f81b9f8da5c1a
Hayato Imai <hayajo@cpan.org>
2013-05-30 02:59:51 +0000

    Import of HAYAJO/Net-IPMessenger-Bot-0.04 from CPAN.
    gitpan-cpan-distribution: Net-IPMessenger-Bot
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         HAYAJO/Net-IPMessenger-Bot-0.04.tar.gz
    gitpan-cpan-author:       HAYAJO
    gitpan-cpan-maturity:     released

--- lib/Net/IPMessenger/Bot/EventHandler.pm
+++ lib/Net/IPMessenger/Bot/EventHandler.pm
@@ -5,6 +5,9 @@ use warnings;
 
 use parent qw/Net::IPMessenger::RecvEventHandler/;
 use Encode qw();
+use IO::Socket;
+use Term::ANSIColor qw(:constants);
+$Term::ANSIColor::AUTORESET = 1;
 
 sub new {
     my ($class, %args) = @_;
@@ -17,6 +20,18 @@ sub new {
         : [ qr// => $args{handler} ];
 
     return $self;
+}
+
+sub debug {
+    my ( $self, $them, $user ) = @_;
+
+    my $peeraddr = inet_ntoa( $them->socket->peeraddr );
+    my $peerport = $them->socket->peerport;
+    my $command  = $them->messagecommand( $user->command );
+    my $modename = $command->modename;
+
+    print CYAN "Received $modename from [$peeraddr:$peerport]";
+    print RESET "\n";
 }
 
 sub handle {
@@ -46,16 +61,24 @@ sub SENDMSG {
     $ipmsg->message([]); #  clear cached-messages
     $self->SUPER::SENDMSG($ipmsg, $user);
 
-    my $res = $self->handle($user);
+    my $command = $ipmsg->messagecommand( $user->command );
+    if ( $command->get_readcheck() ) {
+        $ipmsg->send(
+            {
+                command => $ipmsg->messagecommand('READMSG'),
+                option  => $user->packet_num,
+            }
+        );
+    }
 
-    $ipmsg->send(
-        {
-            command  => $ipmsg->messagecommand('SENDMSG'),
-            peeraddr => $user->peeraddr,
-            peerport => $user->peerport,
-            option   => Encode::encode( 'shiftjis', $res ),
-        }
-    ) if ( defined $res );
+    if ( my $res = $self->handle($user) ) {
+        $ipmsg->send(
+            {
+                command => $ipmsg->messagecommand('SENDMSG'),
+                option  => Encode::encode( 'shiftjis', $res ),
+            }
+        );
+    }
 }
 
 1;
